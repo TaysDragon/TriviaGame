0f909f0bab3c8dd44ffd0c2ad027ba7b18db3726
Dragon <Dragon>
2017-03-24 12:22:20 -0500

    Working timer- still if else

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -70,6 +70,34 @@ function randomQuestion(){
         document.getElementById("randomQuestion");
 }
 //start timer
+var count=30;
+
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     return;
+  }
+
+  //Do code for showing the number of seconds here
+}
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     return;
+  }
+
+ document.getElementById("timer").innerHTML= "Time remaining: " + count + " secs"; // watch for spelling
+}
 
 //compare answer
 // determine if Mathif match inxwewmwn SCORE AND GOOD JOB
